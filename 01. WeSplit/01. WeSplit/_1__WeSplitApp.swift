f700a5dcf22908e94d8cb77bc96910d02f765f44
Kabir Dhillon <kabirdhillon99@gmail.com>
2024-04-08 14:43:47 -0700

    Project 1: WeSplit

---
+++ 01. WeSplit/01. WeSplit/_1__WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1__WeSplitApp.swift
+//  01. WeSplit
+//
+//  Created by Kabir Dhillon on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct _1__WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
