83b7f2a077f7b1af0cf13a1907cbf0a0a6c0108b
Kabir Dhillon <kabirdhillon99@gmail.com>
2024-04-09 21:35:37 -0700

    Project 5: Word Scramble

---
+++ 05. Word Scramble/05. Word Scramble/_5__Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5__Word_ScrambleApp.swift
+//  05. Word Scramble
+//
+//  Created by Kabir Dhillon on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct _5__Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
