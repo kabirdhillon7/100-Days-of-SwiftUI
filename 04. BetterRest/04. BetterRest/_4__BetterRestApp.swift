eb2ae0cec8b965823b7a626587394edd3925756f
Kabir Dhillon <kabirdhillon99@gmail.com>
2024-04-09 19:58:50 -0700

    Project 4: BetterRest

---
+++ 04. BetterRest/04. BetterRest/_4__BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4__BetterRestApp.swift
+//  04. BetterRest
+//
+//  Created by Kabir Dhillon on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct _4__BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
