fc8c4b7dcc08c58466deac7d56001eb25a5393f5
Kabir Dhillon <kabirdhillon99@gmail.com>
2024-05-31 19:12:31 -0700

    Project 15: Accessibility

---
+++ 15. AccessibilitySandbox/15. AccessibilitySandbox/_5__AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5__AccessibilitySandboxApp.swift
+//  15. AccessibilitySandbox
+//
+//  Created by Kabir Dhillon on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct _5__AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
