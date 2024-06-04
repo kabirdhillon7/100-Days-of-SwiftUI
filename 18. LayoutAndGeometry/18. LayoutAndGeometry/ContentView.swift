5a8dfd7fc16e11883c522f894895e60e2de8a61d
Kabir Dhillon <kabirdhillon99@gmail.com>
2024-06-03 22:44:06 -0700

    Project 18: LayoutAndGeometry

---
+++ 18. LayoutAndGeometry/18. LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  18. LayoutAndGeometry
+//
+//  Created by Kabir Dhillon on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 18. LayoutAndGeometry/18. LayoutAndGeometry/_8__LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8__LayoutAndGeometryApp.swift
+//  18. LayoutAndGeometry
+//
+//  Created by Kabir Dhillon on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct _8__LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
